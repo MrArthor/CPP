411af91b927fab9b00e2f32aa8557e33345e70e7
Vansh Sachdev <vksvs25@gmail.com>
2021-07-08 10:12:46 +0530

    Update

--- trial.cpp
+++ trial.cpp
@@ -1,150 +1,258 @@
-/ C++ program to insert a node in AVL tree
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-
+template <class T>
 class Node
 {
-	public:
-	int key;
-	Node *left;
-	Node *right;
-	int height;
+public:
+	Node<T> *lchild;
+	int data;
+	Node<T> *rchild;
 };
-
-
-int max(int a, int b);
-
-
-int height(Node *N)
+template <class T>
+class BST
 {
-	if (N == NULL)
+public:
+	Node<T> *root;
+	BST<T>()
+	{
+		root = NULL;
+	}
+	void Insert(int key)
+	{
+		Node<T> *t = root;
+		Node<T> *r = NULL, *p;
+
+		if (root == NULL)
+		{
+			p = new Node<T>;
+			p->data = key;
+			p->lchild = p->rchild = NULL;
+			root = p;
+			return;
+		}
+		while (t != NULL)
+		{
+			r = t;
+			if (key < t->data)
+				t = t->lchild;
+			else if (key > t->data)
+				t = t->rchild;
+			else
+				return;
+		}
+		p = new Node<T>;
+		p->data = key;
+		p->lchild = p->rchild = NULL;
+
+		if (key < r->data)
+			r->lchild = p;
+		else
+			r->rchild = p;
+	}
+	void Inorder(Node<T> *p)
+	{
+		if (p)
+		{
+			Inorder(p->lchild);
+			cout << p->data << " ";
+			Inorder(p->rchild);
+		}
+	}
+	Node<T> *Search(int key)
+	{
+		Node<T> *t = root;
+
+		while (t != NULL)
+		{
+			if (key == t->data)
+				return t;
+			else if (key < t->data)
+				t = t->lchild;
+			else
+				t = t->rchild;
+		}
+		return NULL;
+	}
+	Node<T> *RInsert(Node<T> *p, int key)
+	{
+		Node<T> *t = NULL;
+
+		if (p == NULL)
+		{
+			t = new Node<T>;
+			t->data = key;
+			t->lchild = t->rchild = NULL;
+			return t;
+		}
+		if (key < p->data)
+			p->lchild = RInsert(p->lchild, key);
+		else if (key > p->data)
+			p->rchild = RInsert(p->rchild, key);
+
+		return p;
+	}
+	int Height(Node<T> *p)
+	{
+		int x, y;
+		if (p == NULL)
+			return 0;
+		x = Height(p->lchild);
+		y = Height(p->rchild);
+		return x > y ? x + 1 : y + 1;
+	}
+	Node<T> *InPre(Node<T> *p)
+	{
+		while (p && p->rchild != NULL)
+			p = p->rchild;
+
+		return p;
+	}
+	Node<T> *InSucc(Node<T> *p)
+	{
+		while (p && p->lchild != NULL)
+			p = p->lchild;
+
+		return p;
+	}
+	Node<T> *Delete(Node<T> *p, int key)
+	{
+		Node<T> *q;
+
+		if (p == NULL)
+			return NULL;
+		if (p->lchild == NULL && p->rchild == NULL)
+		{
+			if (p == root)
+				root = NULL;
+			free(p);
+			return NULL;
+		}
+
+		if (key < p->data)
+			p->lchild = Delete(p->lchild, key);
+		else if (key > p->data)
+			p->rchild = Delete(p->rchild, key);
+		else
+		{
+			if (Height(p->lchild) > Height(p->rchild))
+			{
+				q = InPre(p->lchild);
+				p->data = q->data;
+				p->lchild = Delete(p->lchild, q->data);
+			}
+			else
+			{
+				q = InSucc(p->rchild);
+				p->data = q->data;
+				p->rchild = Delete(p->rchild, q->data);
+			}
+		}
+		return p;
+	}
+	int height(Node<T> *ptr)
+	{
+		int h_left, h_right;
+
+		if (ptr == NULL) /*Base Case*/
+			return 0;
+
+		h_left = height(ptr->lchild);
+		h_right = height(ptr->rchild);
+
+		if (h_left > h_right)
+			return 1 + h_left;
+		else
+			return 1 + h_right;
+	}
+	int isAVL(Node<T> *ptr)
+	{
+		int h_l, h_r, diff;
+		if (ptr == NULL)
+			return 1;
+		h_l = height(ptr->lchild);
+		h_r = height(ptr->rchild);
+		diff = h_l > h_r ? h_l - h_r : h_r - h_l;
+		if (diff <= 1 && isAVL(ptr->lchild) && isAVL(ptr->rchild))
+			return 1;
 		return 0;
-	return N->height;
-}
-
-
-int max(int a, int b)
-{
-	return (a > b)? a : b;
-}
-
-Node* newNode(int key)
-{
-	Node* node = new Node();
-	node->key = key;
-	node->left = NULL;
-	node->right = NULL;
-	node->height = 1; 
-	return(node);
-}
-
-
-Node *rightRotate(Node *y)
-{
-	Node *x = y->left;
-	Node *T2 = x->right;
-
-	x->right = y;
-	y->left = T2;
-
-	y->height = max(height(y->left),
-					height(y->right)) + 1;
-	x->height = max(height(x->left),
-					height(x->right)) + 1;
-
-	return x;
-}
-
-
-Node *leftRotate(Node *x)
-{
-	Node *y = x->right;
-	Node *T2 = y->left;
-
-	y->left = x;
-	x->right = T2;
-
-	x->height = max(height(x->left),
-					height(x->right)) + 1;
-	y->height = max(height(y->left),
-					height(y->right)) + 1;
-
-	return y;
-}
-
-int getBalance(Node *N)
-{
-	if (N == NULL)
-		return 0;
-	return height(N->left) - height(N->right);
-}
-
-Node* insert(Node* node, int key)
-{
-	if (node == NULL)
-		return(newNode(key));
-
-	if (key < node->key)
-		node->left = insert(node->left, key);
-	else if (key > node->key)
-		node->right = insert(node->right, key);
-	else 
-		return node;
-
-	node->height = 1 + max(height(node->left),
-						height(node->right));
-
-	int balance = getBalance(node);
-
-	
-	if (balance > 1 && key < node->left->key)
-		return rightRotate(node);
-
-	if (balance < -1 && key > node->right->key)
-		return leftRotate(node);
-
-	if (balance > 1 && key > node->left->key)
-	{
-		node->left = leftRotate(node->left);
-		return rightRotate(node);
 	}
 
-	if (balance < -1 && key < node->right->key)
+	int getMaxWidth()
 	{
-		node->right = rightRotate(node->right);
-		return leftRotate(node);
+		int maxWidth = 0;
+		int width;
+		int h = Height(root);
+		int i;
+
+		for (i = 1; i <= h; i++)
+		{
+			width = getWidth(root, i);
+			if (width > maxWidth)
+				maxWidth = width;
+		}
+
+		return maxWidth;
 	}
 
-	return node;
-}
-
-
-void preOrder(Node *root)
-{
-	if(root != NULL)
+	int getWidth(Node<T> *child, int level)
 	{
-		cout << root->key << " ";
-		preOrder(root->left);
-		preOrder(root->right);
-	}
-}
 
+		if (root == NULL)
+			return 0;
+
+		if (level == 1)
+			return 1;
+
+		else if (level > 1)
+			return getWidth(root->lchild, level - 1) + getWidth(root->rchild, level - 1);
+	}
+};
 int main()
 {
-	Node *root = NULL;
-	
-	
-	root = insert(root, 10);
-	root = insert(root, 20);
-	root = insert(root, 30);
-	root = insert(root, 40);
-	root = insert(root, 50);
-	root = insert(root, 25);
-	
+	BST<int> Tree;
 
-	cout << "Preorder traversal of the "
-			"constructed AVL tree is \n";
-	preOrder(root);
-	
+	cout << "Options\n\n1. Insert in Binary Search Tree\n\n2.Depth Of Tree\n\n3. Search Node\n\n4. Breath Of The Tree\n\n5. Delete Node";
+	int opt = 1;
+	do
+	{
+		int option;
+		cin >> option;
+		switch (option)
+		{
+		case 1:
+		{
+			cout << "Enter The Leaf Element";
+			int n;
+			cin >> n;
+			Tree.RInsert(Tree.root, n);
+			break;
+		}
+		case 2:
+		{
+			cout << Tree.Height(Tree.root);
+			break;
+		}
+		case 3:
+		{
+			cout << "Enter Element you Have To Search ";
+			int n;
+			cin >> n;
+			Tree.Search(n);
+		}
+		case 4:
+			cout << Tree.getMaxWidth();
+			break;
+		case 5:
+			cout << "Enter Element Ment To Be Deleted ";
+			int n;
+			cin >> n;
+			Tree.Delete(Tree.root, n);
+		default:
+			cout << "Wrong Option ";
+			break;
+		}
+		cout << "Wanna Start Again(press 1) ";
+		cin >> opt;
+	} while (opt == 1);
 	return 0;
 }

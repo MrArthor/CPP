722b6a32445487c82fe25860602e78b9ddb5e87a
Vansh Sachdev <vksvs25@gmail.com>
2021-07-17 10:29:36 +0530

    Update

--- trial2.cpp
+++ trial2.cpp
@@ -1,48 +1,44 @@
 #include <iostream>
-#include <queue>
-
-using std::cout;
-using std::pair;
-using std::priority_queue;
-using std::vector;
-
-int kthSmallest(int mat[4][4], int n, int k)
+using namespace std;
+struct Position
 {
-    auto cmp = [=](std::pair<int, int> a, std::pair<int, int> b)
-    {
-        return mat[a.first][a.second] > mat[b.first][b.second];
-    };
 
-    std::priority_queue<std::pair<int, int>, vector<pair<int, int>>, decltype(cmp)> pq(cmp);
-    for (int i = 0; i < n; i++)
-    {
-        pq.push({i, 0});
-    }
-
-    for (int i = 1; i < k; i++)
-    {
-        auto p = pq.top();
-        pq.pop();
-
-        if (p.second + 1 < n)
-            pq.push({p.first, p.second + 1});
-    }
-    return mat[pq.top().first][pq.top().second];
-}
+	int row, col;
+};
 
 int main()
 {
-    int mat[5][4] = {
-        {12, 21, 32, 43},
-        {16, 24, 36, 45},
-        {25, 30, 37, 49},
-        {32, 34, 39, 52},
-        {35, 36, 42, 55},
-    };
+	int N = 5;
+	//cin>>N;
+	Position Source = {0, 0}, Destination = {4, 4};
+	int Array[N][N] = {{3, 5, 7, 4, 6}, {5, 3, 1, 5, 3}, {2, 8, 3, 1, 4}, {4, 5, 7, 2, 3}, {3, 3, 3, 2, 8}};
+	int StepCount = -1;
+	for (int i = Source.row; i < N; i++)
+	{
 
-    int k;
-    cout << "Enter the value of k: ";
-    std::cin >> k;
-    cout << k << "th smallest element is " << kthSmallest(mat, 4, k);
-    return 0;
+		for (int j = Source.col; j < N; j++)
+		{
+			if (i == Destination.row && j == Destination.col)
+			{
+				break;
+			}
+			else if (i == Destination.row && j > Destination.col)
+			{
+				if (j + Array[i][j] < N)
+				{
+					j += Array[i][j];
+					StepCount++;
+				}
+			}
+			else if (i> Destination.row && j== Destination.col)
+			{
+				if (i + Array[i][j] < N)
+				{
+					i += Array[i][j];
+					StepCount++;
+				}
+			}
+		}
+	}
+	return 0;
 }
--- trial6.cpp
+++ trial6.cpp
@@ -1,82 +1,146 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <set>
+#include <queue>
 using namespace std;
 
-struct HeapNode
+// `N Ã— N` matrix
+#define N 10
+
+// A queue node used in BFS
+struct Node
 {
-    int val;
-    int r;
-    int c;
+	// `(x, y)` represents coordinates of a cell in the matrix
+	int x, y;
+
+	// `parent` stores the parent node of the current cell.
+	// It will have only one entry, i.e., parent node
+	vector<Node> parent;
+
+	// As we are using struct as a key in a `std::set`,
+	// we need to overload the below operators
+
+	bool const operator==(const Node& ob) const {
+		return x == ob.x && y == ob.y;
+	}
+
+	bool operator<(const Node& ob) const {
+		return x < ob.x || (x == ob.x && y < ob.y);
+	}
 };
 
-void swap(HeapNode *x, HeapNode *y)
-{
-    HeapNode z = *x;
-    *x = *y;
-    *y = z;
+// Below arrays detail all four possible movements from a cell
+int row[] = { -1, 0, 0, 1 };
+int col[] = { 0, -1, 1, 0 };
+
+// The function returns false if pt is not a valid position
+bool isValid(int x, int y) {
+	return (x >= 0 && x < N) && (y >= 0 && y < N);
 }
 
-void MinHeapify(HeapNode HeapArr[], int i, int heap_size)
+// Function to print the complete path from source to destination
+int printPath(vector<Node> const &path)
 {
-    int l = i * 2 + 1;
-    int r = i * 2 + 2;
-    int smallest = i;
-    if (l < heap_size && HeapArr[l].val < HeapArr[i].val)
-        smallest = l;
-    if (r < heap_size && HeapArr[r].val == 0)
-    {
-        MinHeapify(HeapArr, i, heap_size);
-        i--;
-    }
+	if (path.size() == 0) {
+		return 0;
+	}
+
+	int len = printPath(path[0].parent) + 1;
+	cout << "(" << path[0].x << ", " << path[0].y << ") ";
+	return len;
 }
 
-int KThSmallest(int mat[][4], int n, int k)
+// Find the shortest route in a matrix from source cell `(x, y)` to
+// destination cell `(N-1, N-1)`
+int findPath(int matrix[][N], int x, int y)
 {
-    if (k < 0 && k >= n * n)
-        return INT_MAX;
+	// create a queue and enqueue the first node
+	queue<Node> q;
+	Node src = {x, y};
+	q.push(src);
 
-    HeapNode HeapArr[n];
-    for (int i = 0; i < n; i++)
-        HeapArr[i] = {mat[0][i], 0, i};
+	// set to check if the matrix cell is visited before or not
+	set<Node> visited;
+	visited.insert(src);
 
-    HeapNode hr;
-    for (int i = 0; i < k; i++)
-    {
-        hr = HeapArr[0];
+	// loop till queue is empty
+	while (!q.empty())
+	{
+		// dequeue front node and process it
+		Node curr = q.front();
+		q.pop();
 
-        int nextval = (hr.r < (n - 1)) ? mat[hr.r + 1][hr.c]   : INT_MAX;
+		int i = curr.x;
+		int j = curr.y;
 
-        HeapArr[0] = {nextval, (hr.r) + 1, hr.c};
+		// if the destination is found, print the shortest path and
+		// return the shortest path length
+		if (i == N - 1 && j == N - 1)
+		{
+			cout << "The shortest path is ";
+			int len = printPath({curr});
+			return len - 1;
+		}
 
-        MinHeapify(HeapArr, 0, n);
-    }
+		// get the value of the current cell
+		int n = matrix[i][j];
 
-    return hr.val;
+		// check all four possible movements from the current cell
+		// and recur for each valid movement
+		for (int k = 0; k < 4; k++)
+		{
+			// get next position coordinates using the value of the current cell
+			int x = i + row[k] * n;
+			int y = j + col[k] * n;
+
+			// check if it is possible to go to the next position
+			// from the current position
+			if (isValid(x, y))
+			{
+				// construct the next cell node
+				Node next = { x, y, {curr} };
+
+				// if it isn't visited yet
+				if (!visited.count(next))
+				{
+					// enqueue it and mark it as visited
+					q.push(next);
+					visited.insert(next);
+				}
+			}
+		}
+	}
+
+	// return -1 if the path is not possible
+	return 0;
 }
 
 int main()
 {
-    // int row, col, k;
-    // cout << "Enter Number Of Columns ";
-    // cin >> col;
-    // cout << "Enter Number Of Rows ";
-    // cin >> row;
-    // int mat[row][4];
-    // cout << "Enter Value Of K ";
-    // cin >> k;
-    // for (int i = 0; i < row; i++)
-    // {
-    //     for (int j = 0; j < col; j++)
-    //     {
-    //         cin >> mat[i][j];
-    //     }
-    // }
-      int mat[4][4] = {
-        { 10, 20, 30, 40 },
-        { 15, 25, 35, 45 },
-        { 25, 29, 37, 48 },
-        { 32, 33, 39, 50 },
-    };
-    cout << "7th smallest element is "
-         << KThSmallest(mat, 4, 7);
-    return 0;
+	int matrix[N][N] =
+	{
+		{ 4, 4, 6, 5, 5, 1, 1, 1, 7, 4 },
+		{ 3, 6, 2, 4, 6, 5, 7, 2, 6, 6 },
+		{ 1, 3, 6, 1, 1, 1, 7, 1, 4, 5 },
+		{ 7, 5, 6, 3, 1, 3, 3, 1, 1, 7 },
+		{ 3, 4, 6, 4, 7, 2, 6, 5, 4, 4 },
+		{ 3, 2, 5, 1, 2, 5, 1, 2, 3, 4 },
+		{ 4, 2, 2, 2, 5, 2, 3, 7, 7, 3 },
+		{ 7, 2, 4, 3, 5, 2, 2, 3, 6, 3 },
+		{ 5, 1, 4, 2, 6, 4, 6, 7, 3, 7 },
+		{ 1, 4, 1, 7, 5, 3, 6, 5, 3, 4 }
+	};
+
+	// Find a route in the matrix from source cell `(0, 0)` to
+	// destination cell `(N-1, N-1)`
+	int len = findPath(matrix, 0, 0);
+
+	if (len != -1) {
+		cout << "\nThe shortest path length is " << len;
+	}
+	else {
+		cout << "Destination not possible";
+	}
+
+	return 0;
 }

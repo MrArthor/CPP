dfa93245be6ace6471950556d336b35080ebd38a
Vansh Sachdev <vksvs25@gmail.com>
2021-10-22 23:51:24 +0530

    Update trial.cpp

--- trial.cpp
+++ trial.cpp
@@ -1,84 +1,30 @@
-#include <stdio.h>
-#include <string.h>
-#include <ctype.h>
-#include <stdlib.h>
-int CalcGCD(int);
+#include <iostream>
+#include <pthread.h>
+using namespace std;
+
+char* str = "Child thread";
+
+void* func(void *str)
+{
+    cout << "Child thread Created: " << (char*)str;
+}
+// main function
 int main()
 {
-     int i, j, k, gcd, Alpha, Beta, NumStringing[100], NumCipher[100];
-     char String[100], cipher[100];
-     printf("Enter a Stringing\n");
-     gets(String);
-     for (i = 0, j = 0; i < strlen(String); i++)
-     {
-          if (String[i] != ' ')
-          {
-               String[j] = toupper(String[i]);
-               j++;
-          }
-          else
-          {
-               String[j] = ' ';
-               j++;
-          }
-     }
-     String[j] = '\0';
-     printf("Entered String is : %s \n", String);
-     printf("Enter Alpha value and must be between 1 and 25 both included\n");
-     scanf("%d", &Alpha);
-     if (Alpha < 1 || Alpha > 25)
-     {
-          printf("Alpha should lie in between 1 and 25\nSorry Try again !\n");
-          exit(0);
-     }
-     gcd = CalcGCD(Alpha);
-     if (gcd != 1)
-     {
-          printf("gcd(Alpha,26)=1 but \n gcd(%d,26)=%d\nSorry Try again !\n", Alpha, gcd);
-          exit(0);
-     }
-     printf("Enter Beta value and must be between 0 and 25 both included\n");
-     scanf("%d", &Beta);
-     if (Beta < 0 || Beta > 25)
-     {
-          printf("Beta value should lie between 0 and 25\nSorry Try again !\n");
-          exit(0);
-     }
-
-     for (i = 0; i < strlen(String); i++)
-     {
-          if (String[i] != ' ')
-               NumStringing[i] = String[i] - 'A';
-          else
-               NumStringing[i] = -20;
-     }
-     printf("Affine Cipher text is\n");
-     for (i = 0; i < strlen(String); i++)
-     {
-          if (NumStringing[i] != -20)
-          {
-               NumCipher[i] = ((Alpha * NumStringing[i]) + Beta) % 26;
-               printf("%c", (NumCipher[i] + 'A'));
-          }
-          else
-          {
-               printf(" ");
-          }
-     }
-     printf("\n");
-}
-
-int CalcGCD(int Alpha)
-{
-     int x;
-     int Temp1 = Alpha;
-     int Temp2 = 26;
-
-     while (Temp2 != 0)
-     {
-          x = Temp2;
-          Temp2 = Temp1 % Temp2;
-          Temp1 = x;
-     }
-     return (Temp1);
+    s = ctime(&Time);
+    // Step 1: Declaring thread
+    pthread_t t;    
+    // Step 2: Calling create thread function
+    pthread_create(&t, NULL, &func, (void*)str); 
+    /*
+        Syntax for pthread_create is:
+        pthread_create(threadID,attr,start_routine,arg)
+        Here,
+        threadID = t, arg = (void*)str, atrr = Null, start_routine = func
+    */
+    cout << "Main thread created" << endl;
+    pthread_join(t, NULL);
+    //Exiting after completion
+    exit(EXIT_SUCCESS); 
+    return 0;
 }

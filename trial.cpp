dd388959a6bd7a5c878cf14a3b4bb05f41ef434f
Vansh Sachdev <vksvs25@gmail.com>
2021-11-06 00:57:48 +0530

    Update

--- trial.cpp
+++ trial.cpp
@@ -1,30 +1,118 @@
-#include <iostream>
-#include <pthread.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-char* str = "Child thread";
-
-void* func(void *str)
+struct Process
 {
-    cout << "Child thread Created: " << (char*)str;
+    int pid;
+    int bt;       
+    int priority; 
+};
+bool comparison(Process a, Process b)
+{
+    return (a.priority > b.priority);
 }
-// main function
+void FirstComeFirstServe(int processes[], int n,
+                         int bt[], int wt[])
+{
+    
+    wt[0] = 0;
+
+    
+    for (int i = 1; i < n; i++)
+        wt[i] = bt[i - 1] + wt[i - 1];
+}
+void RoundRobin(int processes[], int n,
+                int bt[], int wt[], int quantum)
+{
+    int rem_bt[n];
+    for (int i = 0; i < n; i++)
+        rem_bt[i] = bt[i];
+
+    int t = 0;
+    while (1)
+    {
+        bool done = true;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (rem_bt[i] > 0)
+            {
+                done = false;
+                if (rem_bt[i] > quantum)
+                {
+                    t += quantum;
+
+                    rem_bt[i] -= quantum;
+                }
+
+                else
+                {
+                    t = t + rem_bt[i];
+
+                    wt[i] = t - bt[i];
+
+                    rem_bt[i] = 0;
+                }
+            }
+        }
+
+        if (done == true)
+            break;
+    }
+}
+
+void findTurnAroundTime(int processes[], int n,
+                        int bt[], int wt[], int tat[])
+{
+    for (int i = 0; i < n; i++)
+        tat[i] = bt[i] + wt[i];
+}
+
+void findavgTime(int processes[], int n, int bt[],
+                 int quantum)
+{
+    int wt[n], tat[n], total_wt = 0, total_tat = 0;
+
+    RoundRobin(processes, n, bt, wt, quantum);
+
+    findTurnAroundTime(processes, n, bt, wt, tat);
+
+    cout << "Processes "
+         << " Burst time "
+         << " Waiting time "
+         << " Turn around time\n";
+
+    for (int i = 0; i < n; i++)
+    {
+        total_wt = total_wt + wt[i];
+        total_tat = total_tat + tat[i];
+        cout << " " << i + 1 << "\t\t" << bt[i] << "\t "
+             << wt[i] << "\t\t " << tat[i] << endl;
+    }
+
+    cout << "Average waiting time = "
+         << (float)total_wt / (float)n;
+    cout << "\nAverage turn around time = "
+         << (float)total_tat / (float)n;
+}
+
+void priorityScheduling(Process proc[], int n)
+{
+    sort(proc, proc + n, comparison);
+
+    cout << "Order in which processes gets executed \n";
+    for (int i = 0; i < n; i++)
+        cout << proc[i].pid << " ";
+
+}
 int main()
 {
-    s = ctime(&Time);
-    // Step 1: Declaring thread
-    pthread_t t;    
-    // Step 2: Calling create thread function
-    pthread_create(&t, NULL, &func, (void*)str); 
-    /*
-        Syntax for pthread_create is:
-        pthread_create(threadID,attr,start_routine,arg)
-        Here,
-        threadID = t, arg = (void*)str, atrr = Null, start_routine = func
-    */
-    cout << "Main thread created" << endl;
-    pthread_join(t, NULL);
-    //Exiting after completion
-    exit(EXIT_SUCCESS); 
+    int processes[] = {1, 2, 3};
+    int n = sizeof processes / sizeof processes[0];
+
+    int burst_time[] = {10, 5, 8};
+
+    int quantum = 2;
+    findavgTime(processes, n, burst_time, quantum);
     return 0;
 }

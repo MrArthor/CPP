ca277e7d90fbaf29b359188a9bf42b72f5e7a87b
Vansh Sachdev <vksvs25@gmail.com>
2021-12-06 00:12:57 +0530

    Update sever.java

--- sever.java
+++ sever.java
@@ -1,114 +1,77 @@
-ANS4)
-(1)
-package coreservlets; 
-import java.io.*; 
-import javax.servlet.*; 
-import javax.servlet.http.*; 
-import java.net.*;
-public class SearchEngines extends HttpServlet 
-{   
-public void doGet(HttpServletRequest request,HttpServletResponse response)  
- throws ServletException, IOException 
-{  
-String searchString = request.getParameter("searchString");    
-if ((searchString == null)  
- (searchString.length() == 0)) 
-{ 
-reportProblem(response,"Missing search string");       
-return;     
-}     
-searchString = URLEncoder.encode(searchString);     
-String searchEngineName =  request.getParameter("searchEngine");    
- if ((searchEngineName == null) 
-(searchEngineName.length() == 0)) 
-{ 
-reportProblem(response,"Missing search engine name");       
-return;    
-}    
-String searchURL = SearchUtilities.makeURL(searchEngineName, searchString);     
-if (searchURL != null) 
-{
-response.sendRedirect(searchURL);  
-} 
-else 
-{
-reportProblem(response, "Unrecognized search engine");     
-}   
-} 
-private void reportProblem(HttpServletResponse response,               String message)       
-throws IOException 
-{ 
- response.sendError(response.SC_NOT_FOUND, message); 
- } 
-}
+ANS5)
+package com.calendar.reminder.app;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
 
-(2)
-package coreservlets; 
-public class SearchSpec
- {
-private String name, baseURL;   
-public SearchSpec(String name,String baseURL) 
-{
-this.name = name;     
-this.baseURL = baseURL;   
-}  
-public String makeURL(String searchString) 
-{
-return(baseURL + searchString);   
-}   
-public String getName() 
-{ 
-return(name);   
-} 
-}
+import org.apache.commons.lang3.time.DateUtils;
 
-(3)
-package coreservlets; 
-public class SearchUtilities 
-{
-private static SearchSpec[] commonSpecs = { new SearchSpec("Google", "http://www.google.com/search?q="),new SearchSpec("AllTheWeb","http://www.alltheweb.com/search?q="),       new SearchSpec("Yahoo","http://search.yahoo.com/bin/search?p="),       new SearchSpec("AltaVista","http://www.altavista.com/web/results?q="),new SearchSpec("Lycos","search.lycos.com/default.asp?query="),       new SearchSpec("HotBot","http://hotbot.com/default.asp?query="),       new SearchSpec("MSN","http://search.msn.com/results.asp?q="),     };   
-public static SearchSpec[] getCommonSpecs() 
-{
-return(commonSpecs);   
-}  
-public static String makeURL(String searchEngineName,String searchString) 
-{
-SearchSpec[] searchSpecs = getCommonSpecs();     
-String searchURL = null;    
- for(int i=0; i<searchSpecs.length; i++) 
-{
-SearchSpec spec = searchSpecs[i];
-if (spec.getName().equalsIgnoreCase(searchEngineName)) 
-{
-searchURL = spec.makeURL(searchString);         
-break;       
-}    
-}    
-return(searchURL);   
-} 
-}
-(4)
-package coreservlets; 
-import java.io.*; 
-import javax.servlet.*; 
-import javax.servlet.http.*; 
-public class SearchEngineForm extends HttpServlet 
-{   
-public void doGet(HttpServletRequest request, HttpServletResponse response)       
-throws ServletException, IOException 
-{
-response.setContentType("text/html");     
-PrintWriter out = response.getWriter();     
-String title = "One-Stop Web Search!";     
-String actionURL = "/servlet/coreservlets.SearchEngines";     
-String docType = "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 " + "Transitional//EN\">\n";     
-out.println (docType + "<HTML>\n" + "<HEAD><TITLE>" + title + "</TITLE></HEAD>\n" + "<BODY BGCOLOR=\"#FDF5E6\">\n" +        "<CENTER>\n" + "<H1>" + title + "</H1>\n" + "<FORM ACTION=\"" + actionURL + "\">\n" + "  Search keywords: \n" +"  <INPUT TYPE=\"TEXT\" NAME=\"searchString\"><P>\n");     
-SearchSpec[] specs = SearchUtilities.getCommonSpecs();     
-for(int i=0; i<specs.length; i++) 
-{
-String searchEngineName = specs[i].getName();       out.println("<INPUT TYPE=\"RADIO\" " + "NAME=\"searchEngine\" " +  "VALUE=\"" + searchEngineName + "\">\n");       out.println(searchEngineName + "<BR>\n");     
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.DateTime;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.calendar.Calendar;
+import com.google.api.services.calendar.CalendarScopes;
+import com.google.api.services.calendar.model.Events;
 
-}     
-out.println ("<BR>  <INPUT TYPE=\"SUBMIT\">\n" +        "</FORM>\n" + "</CENTER></BODY></HTML>");   
-} 
+public class CalendarReminder {
+    private static final String APPLICATION_NAME = "Calendar Reminder";
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private static final String TOKENS_DIRECTORY_PATH = "tokens";
+
+    private static final List<String> SCOPES = Collections.singletonList(CalendarScopes.CALENDAR_READONLY);
+    private static final String GOOGLE_CREDENTIALS_FILE_PATH = "/credentials.json";
+
+    private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
+        InputStream in = CalendarReminder.class.getResourceAsStream(GOOGLE_CREDENTIALS_FILE_PATH);
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+            HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+            .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+            .setAccessType("offline")
+            .build();
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+    }
+
+    public static void main(String... args) throws IOException, GeneralSecurityException {
+        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        Calendar service = new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+            .setApplicationName(APPLICATION_NAME)
+            .build();
+
+        DateTime now = new DateTime(System.currentTimeMillis());
+        Events events = service.events().list("primary")
+            .setMaxResults(20)
+            .setTimeMin(now)
+            .setOrderBy("startTime")
+            .setSingleEvents(true)
+            .execute();
+        events.getItems().stream()
+        .forEach((e) -> {
+            DateTime eventDate = e.getStart().getDateTime();
+            if (eventDate == null) {
+                eventDate = e.getStart().getDate();
+            }
+            long value = eventDate.getValue();
+            Date eventDateAsJavaObject = new Date(value);
+            Date toBeNotifyDate = DateUtils.addDays(eventDateAsJavaObject, -3);
+            Date today = new Date();
+            if(DateUtils.isSameDay(today, toBeNotifyDate)) {
+                new SendSms().send(e.getSummary());
+            }
+        });
+    }
 }

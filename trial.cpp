83f2c3201056b593b44b347f435e1519ffb9a953
Vansh Sachdev <vksvs25@gmail.com>
2022-04-02 18:23:23 +0530

    Update

--- trial.cpp
+++ trial.cpp
@@ -1,20 +1,20 @@
-#include <iostream> 
-#include <string> 
-#include <regex> 
-#include <iterator> 
-using namespace std; 
-   
-int main() 
-{  
-    string mystr = "This is software testing Help portal \n"; 
-     
-    cout<<"Input string: "<<mystr<<endl;
-       
-    // regex to match string beginning with 'p' 
-    regex regexp("+[a-zA-z]+tion"); 
-    cout<<"Replace the word 'portal' with word 'website' : "; 
-    // regex_replace() for replacing the match with the word 'website'  
-    cout << regex_replace(mystr, regexp, ""); 
-     
-    return 0; 
+#include <iostream>
+
+using namespace std;
+
+int factorial(int n) {
+    if (n == 0) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+int fibonaci
+int main(){
+ cout<<"Enter a number: ";
+ int n;
+ cin>>n;
+ cout<<"Factorial of "<<n<<" is "<<factorial(n)<<endl;
+    cout<<"Hello World";
+    return 0;
 }

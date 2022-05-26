361ba6a479a4c11bfc85b1843f22028677f63d29
Vansh Sachdev <vksvs25@gmail.com>
2022-05-27 01:12:22 +0530

    Update

--- anushka.py
+++ anushka.py
@@ -1,40 +1,23 @@
+import face_recognition
 
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-f = pd.read_csv("people.txt")
-f.index
-len(f)
-def check_age_range(f) :
-    age_range = lambda r : r in range(151)
-    print(f["age"].apply(age_range))
-check_age_range(f)
-def check_age(f) :
-    age_check = lambda r : r[0] > r[1]
-    print(f[["age","yearsmarried"]].apply(age_check,axis=1))
-check_age(f)
-f.status
-def check_status(f) :
-    status=set(f.status)
-    status_check = lambda r : r in status
-    print(f["status"].apply(status_check))
-check_status(f)
-def check_group(f) :
-    def group_check(x) :
-        if (x[0] in range(18) and x[1]=="child") or (x[0] in range(18,66) and x[1]=="adult") or(x[0]>65 and x[1]=="elderly") :
-            return True
-        else :
-            return False
-        print(f[["age","age_group"]].apply(group_check,axis=1))
-check_group(f)
-E = {"check_age_range" : check_age_range, "check_age" : check_age, "check_status":
-check_status, "check_group" : check_group}
-E["check_age"](f)
-#x = []
-#for i in E.keys() :
-# x.append(E[i](f))
-f.loc[0,:]
-x = f.plot()
-#plt.bar(E.keys(),x)
-plt.show()
-f.describe()
+biden_image = face_recognition.load_image_file("biden.jpg")
+obama_image = face_recognition.load_image_file("obama.jpg")
+unknown_image = face_recognition.load_image_file("obama2.jpg")
+try:
+    biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
+    obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+    unknown_face_encoding = face_recognition.face_encodings(unknown_image)[0]
+except IndexError:
+    print("I wasn't able to locate any faces in at least one of the images. Check the image files. Aborting...")
+    quit()
+
+known_faces = [
+    biden_face_encoding,
+    obama_face_encoding
+]
+
+results = face_recognition.compare_faces(known_faces, unknown_face_encoding)
+
+print("Is the unknown face a picture of Biden? {}".format(results[0]))
+print("Is the unknown face a picture of Obama? {}".format(results[1]))
+print("Is the unknown face a new person that we've never seen before? {}".format(not True in results))

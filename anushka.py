33bc02182ac3c97a21fae8a1bb2c7c017c416406
Vansh Sachdev <vksvs25@gmail.com>
2022-06-12 21:10:31 +0530

    Update

--- anushka.py
+++ anushka.py
@@ -1,23 +1,21 @@
-import face_recognition
+s1=['a','b','c']
 
-biden_image = face_recognition.load_image_file("biden.jpg")
-obama_image = face_recognition.load_image_file("obama.jpg")
-unknown_image = face_recognition.load_image_file("obama2.jpg")
-try:
-    biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
-    obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
-    unknown_face_encoding = face_recognition.face_encodings(unknown_image)[0]
-except IndexError:
-    print("I wasn't able to locate any faces in at least one of the images. Check the image files. Aborting...")
-    quit()
+s2= ['b','c']
+def digSum(cls,input1,input2):
+    a=list(set(input1)&set(input2))
+    sum=0
+    n=0
+    for i in a:
+        n+=ord(i)
 
-known_faces = [
-    biden_face_encoding,
-    obama_face_encoding
-]
 
-results = face_recognition.compare_faces(known_faces, unknown_face_encoding)
 
-print("Is the unknown face a picture of Biden? {}".format(results[0]))
-print("Is the unknown face a picture of Obama? {}".format(results[1]))
-print("Is the unknown face a new person that we've never seen before? {}".format(not True in results))
+    if (n == 0):
+        return 0
+    if (n % 9 == 0):
+        return 9
+    else:
+       return (n % 9)
+     
+     
+print(digSum('.',s1,s2))

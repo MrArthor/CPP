a9afe710bcd82bcdcfc62ff3cf98d6418e4a5a11
Vansh Sachdev <vksvs25@gmail.com>
2023-03-06 13:56:18 +0530

    update

--- trial.cpp
+++ trial.cpp
@@ -3,18 +3,18 @@ using namespace std;
  
 // function to convert Celsius
 // scale to Fahrenheit scale
-float Cel_To_Fah(float N)
-{
-    return ((N * 9.0 / 5.0) + 32.0);
-}
- 
+
 // Driver code
 int main()
 {
-    float N = 0;
-   
-      for(int i=0;i<21;i++){
-    cout <<i<<" "<<  Cel_To_Fah(N+i)<<endl;
-      }
+   int y=9;
+   if(y++>9&&y++!=11&&y++>11)
+    {
+         cout<<y;
+    }
+    else
+    {
+        cout<<y;
+    }
     return 0;
 }

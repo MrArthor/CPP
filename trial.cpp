6be46c94885c732f4ecc678819ba72f0bd81eb5a
Vansh Sachdev <vksvs25@gmail.com>
2023-05-14 11:53:53 +0530

    Update

--- trial.cpp
+++ trial.cpp
@@ -1,20 +1,46 @@
-#include <bits/stdc++.h>
-using namespace std;
- 
-// function to convert Celsius
-// scale to Fahrenheit scale
-
-// Driver code
+#include <stdio.h>
+struct student
+{
+    int roll_no;
+    char name[50];
+    int marks;
+} s;
 int main()
 {
-   int y=9;
-   if(y++>9&&y++!=11&&y++>11)
+    int i;
+    int count_1 = 0, count_2 = 0, count_3 = 0, count_4 = 0;
+    for (i = 0; i < 5; i++)
     {
-         cout<<y;
+        printf("enter the details of student:");
+        scanf("%d %s %d", &s.roll_no, &s.name, &s.marks);
     }
-    else
+
+    for (i = 0; i < 5; i++)
     {
-        cout<<y;
+        printf("%d %s %d", &s.roll_no, &s.name, &s.marks);
+    }
+    for (i = 0; i < 5; i++)
+    {
+        if (s.marks >= 60)
+        {
+            count_1++;
+            printf("\nmarks above or equal to 60:%d", count_1);
+        }
+        else if (s.marks < 60 && s.marks >= 50)
+        {
+            count_2++;
+            printf("\nmarks between 50 to 60:%d", count_2);
+        }
+        else if (s.marks < 50 && s.marks >= 40)
+        {
+            count_3++;
+            printf("\nmarks between 40 to 50:%d", count_3);
+        }
+        else if (s.marks < 40)
+        {
+            count_4++;
+            printf("\nmarks less than 40:%d", count_4);
+        }
     }
     return 0;
 }

9bb991d79fab87e001b942ef1d10f8c8b5897e2b
MrArthor <vksvs25@gmail.com>
2024-11-06 12:08:10 +0530

    Update

--- C++/largest-substring-between-two-equal-characters.cpp
+++ C++/largest-substring-between-two-equal-characters.cpp
@@ -1,18 +1,1 @@
-// Time:  O(n)
-// Space: O(1)
-
-class Solution {
-public:
-    int maxLengthBetweenEqualCharacters(string s) {
-        int result = -1;
-        vector<int> lookup(26, -1);
-        for (int i = 0; i < size(s); ++i) {
-            if (lookup[s[i] - 'a'] >= 0) {
-                result = max(result, i - lookup[s[i] - 'a'] - 1);
-            } else {
-                lookup[s[i] - 'a'] = i;
-            }
-        }
-        return result;
-    }
-};
+<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Transitional//EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd'><html xmlns='http://www.w3.org/1999/xhtml'><head><meta content='text/html; charset=utf-8' http-equiv='content-type'/><style type='text/css'>body {font-family:Arial; margin-left:40px; }img  { border:0 none; }#content { margin-left: auto; margin-right: auto }#message h2 { font-size: 20px; font-weight: normal; color: #000000; margin: 34px 0px 0px 0px }#message p  { font-size: 13px; color: #000000; margin: 7px 0px 0px0px}#errorref { font-size: 11px; color: #737373; margin-top: 41px }</style><title>OneDrive</title></head><body><div id='content'><div id='message'><h2>Our services aren't available right now</h2><p>We're working to restore all services as soon as possible. Please check back soon.</p></div><div id='errorref'><span>Ref A: A5257E9330814B528FD7764DA75A2451 Ref B: DEL01EDGE0409 Ref C: 2024-09-11T01:09:16Z</span></div></div></body></html>
